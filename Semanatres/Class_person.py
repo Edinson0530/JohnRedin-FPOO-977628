8cd9788a6c683e6c6bf78da5782555c413b7ec13
John Redin <john.redin@uniminuto.edu.co>
2024-09-26 00:36:44 +0000

    lab_3

---
+++ Semanatres/Class_person.py
@@ -0,0 +1,19 @@
+#----------------
+#person
+
+#dni: int
+#name: str
+#lastname: int
+#role: int
+#----------------
+
+class person:
+    def __init__(self, dni: int, name: str, lastname: int, role: int,):
+        self.dni = dni
+        self.name = Name
+        self.lastname = lastname
+        self.role = role
+
+person1 = person(dni=123, name="Luisitocomunica", lastname="Velez", role=1)
+
+print(person1)
